878a7d41aa106128dfb948e4cb5fd75d80c2d577
JatinShimpi <shimpijatin184@gmail.com>
2024-07-18 02:16:00 +0530

    added dark mode and update user data features

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  //1.load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  //2. if there is no authenticated user, redirect to the /login
+
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  //3.while that is happening show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />;
+      </FullPage>
+    );
+
+  //4. if there is s user render the app
+  if(isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
